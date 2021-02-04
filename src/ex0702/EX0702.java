3984d5a1d27aabbde71fd3384a4b9f4c73e6b4d8
newmind95 <throwexeption95@gmx.com>
2021-02-04 10:01:51 +0200

    Array Exercises

--- src/ex0702/EX0702.java
+++ src/ex0702/EX0702.java
@@ -13,24 +13,24 @@ package ex0702;
 public class EX0702 {
     /** Main method. */
     public static void main(String[] args) {
-        int[] array = new int[10];
-        // Prompt the user to enter ten integers. 
-        System.out.print("Enter ten integers: ");
-        
-        // Fill an array.
-        fill(array);
-        // Display result
-        for (int i = array.length - 1; i >= 0; i--) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
+       int[] array = new int[10];
+       // Prompt the user to enter ten integers.
+       System.out.print("Enter ten integers: ");
+       
+       // Fill an array.
+       fill(array);
+       // Display result.
+       for (int i = array.length - 1; i >= 0; i++) {
+           System.out.print(array[i] + " ");
+       }
+       System.out.println();
     }
     
     /** Method to fill an array. */
     public static void fill(int[] array) {
-        // Create a Scanner.
+        // Create a Scanner
         java.util.Scanner input = new java.util.Scanner(System.in);
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length;i ++) {
             array[i] = input.nextInt();
         }
     }
